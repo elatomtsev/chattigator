373461dfa85f6db99913daf0680c3704c6d37a7b
elatomtsev <o-elatomtsev-m@yandex.ru>
2025-01-02 20:41:16 +0300

    edit filters

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from database import DateBase
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message, ContentType
-from aiogram.filters import Command
+from aiogram.filters import Command, or_f
 
 bot = Bot(token=botyara.bot_token)
 dp = Dispatcher()
@@ -23,25 +23,17 @@ async def ping_all(message: Message):
     await bot.pin_chat_message(chat_id=message.chat.id, message_id=message.message_id)
 
 
-@dp.message(F.text)
+# @dp.message(F.content_type.in_({ContentType.TEXT}))
+@dp.message()
 async def any_messages(message: Message):
     for data in db.select_data("telegram"):
         if message.from_user.id in data:
             break
     else:
-        try:
-            fullname = message.from_user.full_name
-        except:
-            fullname = None
-        try:
-            username = message.from_user.username
-        except:
-            username = None
-
         db.insert_data(
             telegram=message.from_user.id,
-            fullname=fullname,
-            username=username,
+            fullname=message.from_user.full_name,
+            username=message.from_user.username,
         )
 
 
