aff9a21344a3c5406b33e54446d0db444a654747
elatomtsev <o-elatomtsev-m@yandex.ru>
2025-01-07 22:56:01 +0300

    теперь каждое сообщение пользователя заносит обновленные ИМЯ и ЮЗЕРНЕЙМ

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ from aiogram.filters import Command, or_f
 
 bot = Bot(token=botyara.bot_token)
 dp = Dispatcher()
-db = DateBase()
+db = DateBase("database.db")
 
 
 @dp.message(Command(commands="all", prefix=["@", "/"]))
 async def ping_all(message: Message):
     text = []
     for id, telegram, fullname, username in db.select_data():
+        if fullname == "CHATTIGATOR":
+            continue
         text.append(f"[{fullname}](tg://user?id={telegram})")
+
     await message.reply(
         # chat_id=message.chat.id,
         text=", ".join(text),
@@ -23,11 +26,16 @@ async def ping_all(message: Message):
     await bot.pin_chat_message(chat_id=message.chat.id, message_id=message.message_id)
 
 
-# @dp.message(F.content_type.in_({ContentType.TEXT}))
 @dp.message()
 async def any_messages(message: Message):
-    for data in db.select_data("telegram"):
+    for data in db.select_data(["telegram"]):
         if message.from_user.id in data:
+            db.update_data(
+                "telegram",
+                message.from_user.id,
+                fullname=message.from_user.full_name,
+                username=message.from_user.username,
+            )
             break
     else:
         db.insert_data(
@@ -45,5 +53,4 @@ if __name__ == "__main__":
         fullname="TEXT",
         username="TEXT",
     )
-
     dp.run_polling(bot)
