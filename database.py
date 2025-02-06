9c2eb717914b72b5aefa2e83eee8b65067bfa37c
elatomtsev <o-elatomtsev-m@yandex.ru>
2025-02-06 17:01:56 +0300

    появился новый параметр: name_table - теперь не часть класа, при вызове функций, работающих с таблицами, необходимо указать навзвание таблицы

--- database.py
+++ database.py
@@ -4,62 +4,62 @@ import sqlite3
 class DateBase:
     def __init__(self, name_db):
         self.name_db = name_db
-        self.open_table()
+        self.open_db()
 
-    def open_table(self):
+    def open_db(self):
         # Подключаеися к БД
         self.connection = sqlite3.connect(self.name_db)
         self.cursor = self.connection.cursor()
         self.connection.commit()
 
     def create_table(self, name_table: str, **column: str):
-        self.name_table = name_table
+        #self.name_table = name_table
 
         # Добавляем колонки с именем и типом
         cols = ",\n".join([f"{name} {type}" for name, type in column.items()])
 
         # Создаем таблицу, если она не создана
-        req = f"""CREATE TABLE IF NOT EXISTS {self.name_table} (\n{cols})"""
+        req = f"""CREATE TABLE IF NOT EXISTS {name_table} (\n{cols})"""
         self.cursor.execute(req)
         self.connection.commit()
 
-    def insert_data(self, **row: str):
+    def insert_data(self, name_table: str, **row: str):
         # Формируем строчку
         names = ", ".join(row.keys())
         values = ", ".join(["?" for i in range(len(row.values()))])
 
         # Добавляем данные: тг айди, полное имя, юзернейм
-        req = f"INSERT INTO {self.name_table} ({names}) VALUES ({values})"
+        req = f"INSERT INTO {name_table} ({names}) VALUES ({values})"
         self.cursor.execute(req, tuple(row.values()))
 
         self.connection.commit()
 
-    def select_data(self, column: list[str] = "*"):
+    def select_data(self, name_table: str, column: list[str] = "*"):
         # Выбираем колонки, которые будем парсить
         column = ", ".join(column)
 
         # Возвращаем данные
-        req = f"SELECT {column} from {self.name_table}"
+        req = f"SELECT {column} from {name_table}"
         self.cursor.execute(req)
 
         self.connection.commit()
 
         return self.cursor.fetchall()
 
-    def update_data(self, **kwargs: str):
+    def update_data(self, name_table: str, **kwargs: str):
         # Формируеи строку замены
         new_data = ", ".join([f"{column} = ?" for column in list(kwargs.keys())[1:]])
         # Сохраняем название колонки и данные, которые надо заменить
         name_column, need_data = list(kwargs.items())[0]
 
         req = (
-            f"UPDATE {self.name_table} SET {new_data} WHERE {name_column} = {need_data}"
+            f"UPDATE {name_table} SET {new_data} WHERE {name_column} = {need_data}"
         )
         self.cursor.execute(req, tuple(kwargs.values())[1:])
 
         self.connection.commit()
 
-    def close_table(self):
+    def close_db(self):
         self.connection.commit()
         self.connection.close()
 
