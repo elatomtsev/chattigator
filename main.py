a0cece4a2154641b51eba66ab8f8573a22b551a5
elatomtsev <o-elatomtsev-m@yandex.ru>
2025-02-06 17:00:59 +0300

    добавлено разделение по чатам: теперь каждый чат - самостояльная таблица, также добавлена фунция проверки на существования уже вставленных данных

--- main.py
+++ main.py
@@ -10,17 +10,30 @@ dp = Dispatcher()
 db = DateBase("database.db")
 
 
+def check_exist_data(name_table: str, message: Message, column: list[str]) -> bool:
+    # Извлекаем все telegram_id и ищем есть необходимый
+    for telegram in db.select_data(name_table, ["telegram"]):
+        if message.chat.id in telegram:
+            return True
+    return False
+
+
 @dp.message(Command(commands="all", prefix=["@", "/"]))
 async def ping_all(message: Message):
+    # Добавляем chat к telegram_id, удалив минус, потому что sqlite не позволяет создать таблицу, где есть число и минус
+    chat_id = "chat" + str(message.chat.id).strip("-")
+
+    # Сюда будем складывать строки с ссылками на участников для их отметки
     text = []
-    for id, telegram, fullname, username in db.select_data():
+
+    for id, telegram, fullname, username in db.select_data(chat_id):
         # Не отмечать бота
         if telegram == bot.id:
             continue
         text.append(f"[{fullname}](tg://user?id={telegram})")
 
+    # Отмечаем всех участников
     await message.reply(
-        # chat_id=message.chat.id,
         text=", ".join(text),
         parse_mode="Markdown",
     )
@@ -28,20 +41,46 @@ async def ping_all(message: Message):
 
 
 @dp.message()
-async def any_messages(message: Message):
-    # Извлекаем телеграм айди из таблицы
-    for data in db.select_data(["telegram"]):
-        # Если сообщение от существующего участника, то обновляем информацию
-        if message.from_user.id in data:
-            db.update_data(
-                telegram=message.from_user.id,
-                fullname=message.from_user.full_name,
-                username=message.from_user.username,
-            )
-            break
-    # Если новый участник (первое сообщение), то добавляем в таблицу
-    else:
+async def any_message(message: Message):
+    # Добавляем chat к telegram_id, удалив минус, потому что sqlite не позволяет создать таблицу, где есть число и минус
+    chat_id = "chat" + str(message.chat.id).strip("-")
+
+    # Создаём таблицу, если она не существует, с участниками чата
+    db.create_table(
+        chat_id,
+        id="INTEGER PRIMARY KEY",
+        telegram="INTEGER",
+        fullname="TEXT",
+        username="TEXT",
+    )
+
+    # Если чат уже есть в таблице, то обновляем данные (может быть, что-то изменилось)
+    if not check_exist_data("Chatters", message, ["telegram"]):
         db.insert_data(
+            "Chatters",
+            telegram=message.chat.id,
+            name=message.chat.full_name,
+            username=message.chat.username,
+        )
+    else:
+        db.update_data(
+            "Chatters",
+            telegram=message.chat.id,
+            name=message.chat.full_name,
+            username=message.chat.username,
+        )
+
+    # Если участник есть в таблице, то обновляем его данные
+    if not check_exist_data(chat_id, message, ["telegram"]):
+        db.insert_data(
+            chat_id,
+            telegram=message.from_user.id,
+            fullname=message.from_user.full_name,
+            username=message.from_user.username,
+        )
+    else:
+        db.update_data(
+            chat_id,
             telegram=message.from_user.id,
             fullname=message.from_user.full_name,
             username=message.from_user.username,
@@ -53,7 +92,7 @@ if __name__ == "__main__":
         "Chatters",
         id="INTEGER PRIMARY KEY",
         telegram="INTEGER",
-        fullname="TEXT",
+        name="TEXT",
         username="TEXT",
     )
     dp.run_polling(bot)
